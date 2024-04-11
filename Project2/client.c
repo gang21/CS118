0b4c68ad1555600e1b9b1dc2ce2c82f846c19fe9
Gabriella Ang <gabriella90631@gmail.com>
2024-04-11 00:22:20 -0700

    moving files

---
+++ Project2/client.c
@@ -0,0 +1,134 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "utils.h"
+
+int main(int argc, char *argv[]) {
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in client_addr, server_addr_to, server_addr_from;
+    socklen_t addr_size = sizeof(server_addr_to);
+    struct packet pkt;
+    struct packet ack_pkt;
+    // struct packet ack_pkt;
+    char buffer[PAYLOAD_SIZE];
+    unsigned short seq_num = 0;
+    unsigned short ack_num = 0;
+    int last = 0;
+    // char ack = 0;
+    struct timeval  timeout;
+    int n;
+    int flag = 0;
+
+    // read filename from command line argument
+    if (argc != 2) {
+        printf("Usage: ./client <filename>\n");
+        return 1;
+    }
+    char *filename = argv[1];
+
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
+
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
+    }
+
+    // Configure the server address structure to which we will send data
+    memset(&server_addr_to, 0, sizeof(server_addr_to));
+    server_addr_to.sin_family = AF_INET;
+    server_addr_to.sin_port = htons(SERVER_PORT_TO);
+    server_addr_to.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Configure the client address structure
+    memset(&client_addr, 0, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(CLIENT_PORT);
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the listen socket to the client address
+    if (bind(listen_sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
+
+    // Open file for reading
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        close(listen_sockfd);
+        close(send_sockfd);
+        return 1;
+    }
+
+    // TODO: Read from file, and initiate reliable data transfer to the server
+    while(flag == 0) {
+        //read from buffer
+        long reset_pos = ftell(fp);
+        size_t bytesRead = fread(buffer, 1, PAYLOAD_SIZE, fp);
+        
+        //check last packet
+        if (bytesRead < PAYLOAD_SIZE) {
+            last = 1;
+        }
+        else {
+            last = 0;
+        }
+        //build packet
+        build_packet(&pkt, seq_num, seq_num, last, 0, bytesRead, buffer);
+
+        if (sendto(send_sockfd, &pkt, sizeof(pkt), 0, (struct sockaddr*)&server_addr_to, sizeof(server_addr_to)) < 0) {
+            perror("Error sending to server\n");
+            break;
+        }
+
+        // set timeout
+        timeout.tv_sec = 0; 
+        timeout.tv_usec = 500000;
+        setsockopt(listen_sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
+        n = recvfrom(listen_sockfd, &ack_pkt, sizeof(ack_pkt), 0, (struct sockaddr*)&server_addr_from, &addr_size);
+        if (n < 0) {
+            fseek(fp, reset_pos, SEEK_SET);
+            continue;
+        }
+
+        if (ack_pkt.acknum == seq_num && ack_pkt.ack) {
+            ack_num = 1;
+        }
+        else {
+            ack_num = 0;
+        }
+        //expected ack is correct 
+        if (ack_num) {
+            seq_num++;
+        }
+        else {      // expected ack is incorrect
+            fseek(fp, reset_pos, SEEK_SET);
+            continue;
+        }
+
+        if (last == 1) {
+            flag = 1;
+        }
+    }
+
+    printf("[SUCCES] Sending file to server\n");
+    printf("[CLOSING] Disconnecting from server\n");
+    
+    fclose(fp);
+    close(listen_sockfd);
+    close(send_sockfd);
+    return 0;
+}
+
---
+++ Project2/server.c
@@ -0,0 +1,102 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+int main() {
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in server_addr, client_addr_from, client_addr_to;
+    // struct packet buffer;
+    socklen_t addr_size = sizeof(client_addr_from);
+    struct packet ack_pkt;
+    struct packet send_ack;
+    unsigned short seq = 0;
+    int n;
+
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
+    }
+
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
+
+    // Configure the server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the listen socket to the server address
+    if (bind(listen_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
+
+    // Configure the client address structure to which we will send ACKs
+    memset(&client_addr_to, 0, sizeof(client_addr_to));
+    client_addr_to.sin_family = AF_INET;
+    client_addr_to.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+    client_addr_to.sin_port = htons(CLIENT_PORT_TO);
+
+    // Open the target file for writing (always write to output.txt)
+    FILE *fp = fopen("output.txt", "wb");
+
+    // TODO: Receive file from the client and save it as output.txt
+    while(1) {
+        //receive from client
+        n = recvfrom(listen_sockfd, &ack_pkt, sizeof(ack_pkt), 0, (struct sockaddr*)&client_addr_from, &addr_size);
+        if (n < 0) {
+            perror("Error sending ACK to the client");
+            return -1;
+        }
+
+        //check if correct ack -- write to file
+        printf("seq num: %d\n", ack_pkt.seqnum);
+        if (ack_pkt.seqnum == seq) {
+            // write to output.txt
+            fwrite(ack_pkt.payload, 1, ack_pkt.length, fp);
+            //send ack
+            build_packet(&send_ack, ack_pkt.seqnum, ack_pkt.seqnum, 0, 1, 0, NULL);
+            n = sendto(send_sockfd, &send_ack, sizeof(send_ack), 0, (struct sockaddr*)&client_addr_to, sizeof(client_addr_to));
+            if (n < 0) {
+                perror("Error sending ACK to client\n");
+                break;
+            }
+            //increment seq num
+            seq++;
+
+        }
+        else {      // wrong packet
+            build_packet(&send_ack, seq-1, seq-1, 0, 1, 0, NULL);
+            n = sendto(send_sockfd, &send_ack, sizeof(send_ack), 0, (struct sockaddr*)&client_addr_to, sizeof(client_addr_to));
+            if (n < 0) {
+                perror("Error sending ACK to client\n");
+                break;
+            }
+        }
+        //check if last packet
+        if (ack_pkt.last == 1) {
+            printf("last! \n");
+            break;
+        }
+    }
+
+    printf("[SUCCES] File Transfer Complete\n");
+    printf("[CLOSING] Closing server\n");
+
+    fclose(fp);
+    close(listen_sockfd);
+    close(send_sockfd);
+    return 0;
+}
